4e5ca92cffe0f9f74d49f475781278d5a2a318be
Arianna Bunnell <ariannabunnell@gmail.com>
2019-02-06 10:11:57 -0700

    adding files into templates folder

--- blog_2610/blog/views.py
+++ blog_2610/blog/views.py
@@ -7,4 +7,8 @@ def index(request):
 
 # Create your views here.
 
+def index(request):
+    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #context = {'latest_question_list': latest_question_list}
+    return render(request, 'blog/index.html')
 
