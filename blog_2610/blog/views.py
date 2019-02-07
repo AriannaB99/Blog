b7477e677c33c80b29057b98cca14d3396b89578
Arianna Bunnell <ariannabunnell@gmail.com>
2019-02-06 19:18:35 -0700

    404 error when i try to bring up the pages in my browser

--- blog_2610/blog/views.py
+++ blog_2610/blog/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello world. You're at the blog index")
-
-
-# Create your views here.
+from time import strftime
 
 def index(request):
-    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    #context = {'latest_question_list': latest_question_list}
-    return render(request, 'blog/index.html')
+    return render(request, 'blog/index.html', { 'now': strftime('%c')})
 
+
+highFives = 0
+def highFive(request):
+    # Bear with me, I don't have a database yet... :(
+    global highFives
+    highFives += 1
+    return render(request, 'hello/highFive.html', { 'highFives': highFives })
