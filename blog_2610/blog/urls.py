8782ac1ba820a6f7a9638661be132bc29af42664
Arianna Bunnell <ariannabunnell@gmail.com>
2019-02-22 14:38:52 -0700

    just need to add the comment form, styling all looks the way i want it to

--- blog_2610/blog/urls.py
+++ blog_2610/blog/urls.py
@@ -6,9 +6,13 @@ app_name = 'blog'
 urlpatterns = [
 
     path('', views.index, name='index'),
+    #path('detail', views.DetailView.as_view(), name = 'detail'),
     path('bio', views.bio, name='bio'),
     path('techtips', views.techtips, name='techtips'),
     path('biohome', views.BiohomeView.as_view(), name = 'biohome'),
+    path('archive', views.ArchiveView.as_view(), name = 'archive'),
+    path('biohome/<int:pk>', views.BlogDetailView.as_view(), name='blog_detail'),
+#url(r'post/(?P<post_id>\d+)/$', GenreDetail.as_view(), name = 'post_detail'),
 ]
 
 
--- blog_2610/blog/views.py
+++ blog_2610/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
-from .models import Blog
+from .models import Blog, Comments
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
@@ -30,9 +30,22 @@ class BiohomeView(generic.ListView):
     model = Blog
     template_name = 'blog/biohome.html'
     context_object_name = 'latest_post_list'
-
-
-
     def get_queryset(self):
-        return Blog.objects.order_by('-date_posted')[:5]
+        return Blog.objects.order_by('-date_posted')[:2]
 
+class ArchiveView(generic.ListView):
+    model = Blog
+    template_name = 'blog/archive.html'
+    context_object_name = 'all_posts'
+    def get_queryset(self):
+        return Blog.objects.order_by('-date_posted')
+
+class BlogDetailView(generic.DetailView):
+    model =  Blog
+    template_name = "blog/blog_detail.html"
+    #template_name = "blog/detail.html"
+
+    '''class BookDetailView(generic.DetailView):
+    model = Book'''
+    #def get_queryset(self):
+     #   return Comments.objects.filter('blog_id' = )
