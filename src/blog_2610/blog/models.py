4f80fe0c2dc5438c2a386537b71cc194011827fe
Arianna Bunnell <ariannabunnell@gmail.com>
2019-02-26 09:46:28 -0700

    Added src folder

---
+++ src/blog_2610/blog/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=30) #user designated title
+    author_name = models.CharField(max_length=30) #who posted it
+    content = models.TextField(max_length=500) # what is actually in the blog post
+    date_posted = models.DateTimeField(auto_now_add=True) #this is going to add the current date/time of when the post
+                                                    #was created to our blog post
+    #pub_date = models.DateTimeField("date published")
+    def __str__(self):
+        return self.title
+
+class Comments(models.Model):
+    blog_id = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    content = models.TextField(max_length=500)
+    commenter = models.CharField(max_length=30)
+    email_address = models.EmailField(blank = True)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.commenter
+
+
+
+
+
+    #Want to add something to check that the emdateail they enter is valid,
+    #something like try:
+    #validate_email("foo.bar@baz.qux")
+#except ValidationError as e:
+   # print "oops! wrong email"
+#else:
+   # print "hooray! email is valid"
+   #which seems to be secure, and I think would be a cool validation to have
---
+++ src/blog_2610/blog/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from . import views
+from django import urls
+
+app_name = 'blog'
+
+urlpatterns = [
+
+    #path('', views.index, name='index'),
+    #path('detail', views.DetailView.as_view(), name = 'detail'),
+    path('bio', views.bio, name='bio'),
+    path('techtips', views.techtips, name='techtips'),
+    path('', views.BiohomeView, name = 'biohome'),
+    path('archive', views.ArchiveView, name = 'archive'),
+    path('biohome/<int:question_id>', views.BlogDetailView, name='blog_detail'),
+    path('<int:question_id>/add_comment/', views.add_comment, name='add_comment'),
+    path('init', views.init, name='init'),
+    #path('biohome/add_comment', views.add_comment, name='add_comment'),  # NEW MAPPING!
+]
+
+
+
